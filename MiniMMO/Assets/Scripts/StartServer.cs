593c7225628fdf02828ae257845c5e61d7604be0
Oliver Grimm <olivergrimm89@gmail.com>
2025-03-05 14:03:12 +0100

    Disabled SceneManagement + added StartServer + StartClient + TMP

--- MiniMMO/Assets/Scripts/StartServer.cs
+++ MiniMMO/Assets/Scripts/StartServer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-public class StartClient : MonoBehaviour
+public class StartServer : MonoBehaviour
 {
     public void Start()
     {
