804b6013d881e0d1a233673b18c0f99c94caff71
Oliver Grimm <olivergrimm89@gmail.com>
2025-03-06 19:21:41 +0100

    EmoteSystem 🗿

---
+++ MiniMMO/Assets/Scripts/EmoteController.cs
@@ -0,0 +1,62 @@
+using Unity.Netcode;
+using UnityEngine;
+using System.Collections;
+
+public class EmoteController : NetworkBehaviour
+{
+    public GameObject emotePrefab;
+    private GameObject currentEmote;
+    public float emoteDuration = 3f;
+
+    private void Update()
+    {
+        if (IsOwner && Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            DisplayEmoteServerRpc();
+        }
+    }
+    [ServerRpc]
+    private void DisplayEmoteServerRpc(ServerRpcParams rpcParams = default)
+    {
+        DisplayEmoteClientRpc();
+
+        DestroyEmoteServerRpc();
+    }
+
+    [ClientRpc]
+    private void DisplayEmoteClientRpc()
+    {
+        if (currentEmote == null)
+        {
+            currentEmote = Instantiate(emotePrefab, transform.position + new Vector3(0, 2, 0), Quaternion.identity);
+            currentEmote.transform.SetParent(transform); 
+
+            StartCoroutine(DestroyEmoteAfterDelay());
+        }
+    }
+
+    private IEnumerator DestroyEmoteAfterDelay()
+    {
+        yield return new WaitForSeconds(emoteDuration);
+
+        if (currentEmote != null)
+        {
+            Destroy(currentEmote);
+        }
+    }
+
+    [ServerRpc]
+    private void DestroyEmoteServerRpc(ServerRpcParams rpcParams = default)
+    {
+        DestroyEmoteClientRpc();
+    }
+
+    [ClientRpc]
+    private void DestroyEmoteClientRpc()
+    {
+        if (currentEmote != null)
+        {
+            Destroy(currentEmote);
+        }
+    }
+}
